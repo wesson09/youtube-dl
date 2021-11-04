d6aedf91ae4ca9fc01d1fb05419ca6f8c749b4f4
julien <julien@vso-software.fr>
2021-11-04 09:12:49 +0100

    finish extractor

--- youtube_dl/extractor/tvhai.py
+++ youtube_dl/extractor/tvhai.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 from __future__ import unicode_literals
-
+#import brotli
 import time
 import http.client
 import json
 import re
+
+import base64
 from typing import *
 from urllib import parse
 from ssl import SSLSocket
@@ -45,6 +47,42 @@ from aioquic.quic.events import QuicEvent
 from aioquic.tls import CipherSuite, SessionTicket
 #import M2Crypto
 #import OpenSSL
+
+from threading import Thread
+class HeadRequest(Thread,InfoExtractor):
+    headrequeststring = 0
+    video_id=''
+    headers={}
+    responseheader='';
+
+    def __init__(self, args,vid,h,extract):
+        Thread.__init__(self)
+
+        super(InfoExtractor,self).__init__();
+        self.    headrequeststring  = args
+        self.    video_id  = vid
+        self.    headers  = h
+        self.    extract  = extract
+
+    # The run method is overridden to define
+    # the thread body
+    def run(self):
+        fail=True;
+        nbtry=0;
+        while fail and nbtry<10:
+            try:
+               self.responseheader = self._request_webpage(self.headrequeststring, self.video_id,  # 'note', 'errnote', fatal, data=data,
+                                              headers=self.headers,
+                                              #   query=query, expected_status=expected_status
+                                              )
+               print(self.responseheader.headers['content-length'])
+               fail=False;
+            except:
+                fail=True;
+                time.sleep(1);
+            finally:
+                nbtry=nbtry+1;
+
 async def runhttp3request(
         configuration: QuicConfiguration,
         urls: List[str],
@@ -125,7 +163,7 @@ async def perform_http_request(
         http_events = await client.post(
             url,
             data=data.encode(),
-            headers={"content-type": "application/x-www-form-urlencoded"},
+             headers={"content-type": "application/x-www-form-urlencoded"},
         )
         method = "POST"
     else:
@@ -204,8 +242,8 @@ class TVhaiIE(InfoExtractor):#TurnerBaseIE):
         'info_dict': {
             'id': 'rQxZvXQ4ROaSOqq-or2Mow',
             'ext': 'mp4',
-            'title': 'Rick and Morty - Pilot',
-            'description': 'Rick moves in with his daughter\'s family and establishes himself as a bad influence on his grandson, Morty.',
+            'title': 'Rot',
+            'description': 'R.',
             'timestamp': 1543294800,
             'upload_date': '20181127',
         },
@@ -214,71 +252,17 @@ class TVhaiIE(InfoExtractor):#TurnerBaseIE):
             'skip_download': True,
         },
         'expected_warnings': ['Unable to download f4m manifest'],
-    }, {
-        'url': 'http://www.adultswim.com/videos/tim-and-eric-awesome-show-great-job/dr-steve-brule-for-your-wine/',
-        'info_dict': {
-            'id': 'sY3cMUR_TbuE4YmdjzbIcQ',
-            'ext': 'mp4',
-            'title': 'Tim and Eric Awesome Show Great Job! - Dr. Steve Brule, For Your Wine',
-            'description': 'Dr. Brule reports live from Wine Country with a special report on wines.  \nWatch Tim and Eric Awesome Show Great Job! episode #20, "Embarrassed" on Adult Swim.',
-            'upload_date': '20080124',
-            'timestamp': 1201150800,
-        },
-        'params': {
-            # m3u8 download
-            'skip_download': True,
-        },
-        'skip': '404 Not Found',
-    }, {
-        'url': 'http://www.adultswim.com/videos/decker/inside-decker-a-new-hero/',
-        'info_dict': {
-            'id': 'I0LQFQkaSUaFp8PnAWHhoQ',
-            'ext': 'mp4',
-            'title': 'Decker - Inside Decker: A New Hero',
-            'description': 'The guys recap the conclusion of the season. They announce a new hero, take a peek into the Victorville Film Archive and welcome back the talented James Dean.',
-            'timestamp': 1469480460,
-            'upload_date': '20160725',
-        },
-        'params': {
-            # m3u8 download
-            'skip_download': True,
-        },
-        'expected_warnings': ['Unable to download f4m manifest'],
-    }, {
-        'url': 'http://www.adultswim.com/videos/attack-on-titan',
-        'info_dict': {
-            'id': 'attack-on-titan',
-            'title': 'Attack on Titan',
-            'description': 'md5:41caa9416906d90711e31dc00cb7db7e',
-        },
-        'playlist_mincount': 12,
-    }, {
-        'url': 'http://www.adultswim.com/videos/streams/williams-stream',
-        'info_dict': {
-            'id': 'd8DEBj7QRfetLsRgFnGEyg',
-            'ext': 'mp4',
-            'title': r're:^Williams Stream \d{4}-\d{2}-\d{2} \d{2}:\d{2}$',
-            'description': 'original programming',
-        },
-        'params': {
-            # m3u8 download
-            'skip_download': True,
-        },
-        'skip': '404 Not Found',
-    }]
+    },
+    ]
 
 
     def _real_extract(self, url):
         show_path = re.match(self._VALID_URL, url).groups()[2]
         #print(show_path);
 
-
-
         webpage=self._download_webpage(url,show_path);
         propvalpattern='<\s*meta\s+property\s*=\s*\"og:(?P<prop>[\w^\"]+)\"\s+content\s*=\s*\"(?P<value>[\w\W]|[^\"]*)\"\s*\/\s*>'
-        matches = re.finditer(
-            propvalpattern,
-            webpage)
+        matches = re.finditer( propvalpattern,   webpage)
         ogtitle='';
         ogdescription='';
         for m in matches:
@@ -286,14 +270,14 @@ class TVhaiIE(InfoExtractor):#TurnerBaseIE):
                 ogtitle=m.group(2);
             elif m.group(1) == 'description' :
                 ogdescription=m.group(2);
-        
+
         matches = re.findall(
         '(?:https?:)?\/\/play\.tvhaystream.xyz\/play\/v1\/(?P<ID>\w+)',
         webpage)
 
         if not matches :#embed video from other site
             return;
-        video_id= matches[0];  
+        video_id= matches[0];
         prejsonwebpage=self._download_webpage('https://play.tvhaystream.xyz/play/v1/%s' %video_id,video_id,headers={
            # 'Host': 'api-if.tvhaystream.xyz',
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
@@ -313,8 +297,8 @@ class TVhaiIE(InfoExtractor):#TurnerBaseIE):
                 'Origin': '%s' % ('https://play.tvhaystream.xyz'),
             });
        # print(prejsonwebpage);
-        
-        
+
+
     #     var TYPEEND = 'html';
     #var DOMAIN_API = 'https://api-plhq.tvhaystream.xyz/apiv4/';
     #var DOMAIN_LIST_RD = ["plhq01.ggcctvhai001.xyz","plhq02.ggcctvhai001.xyz","plhq03.ggcctvhai001.xyz","plhq04.ggcctvhai001.xyz","plhq05.ggcctvhai001.xyz","plhq06.ggcctvhai001.xyz","plhq07.ggcctvhai001.xyz","plhq08.ggcctvhai001.xyz"];
@@ -323,72 +307,199 @@ class TVhaiIE(InfoExtractor):#TurnerBaseIE):
         #print(matches);
         #idfile=matches[0]
         #assert( idfile == videoid)
-        
-        matches = re.findall('var idUser = "(?P<ID>\w+)"',prejsonwebpage);        
+
+        matches = re.findall('var idUser = "(?P<ID>\w+)"',prejsonwebpage);
         #  print(matches);
         idUser=matches[0]
-        matches = re.findall('var DOMAIN_API = \'(?P<ID>[\w:\/.-]+)\'',prejsonwebpage);        
+        matches = re.findall('var DOMAIN_API = \'(?P<ID>[\w:\/.-]+)\'',prejsonwebpage);
         #print(matches);
-        DOMAIN_API=matches[0]
-        
-        #print( '%s%s/%s' % (DOMAIN_API,idUser,video_id));
-        context = ssl._create_default_https_context()
-        # enable PHA for TLS 1.3 connections if available
-        #if context.post_handshake_auth is not None:
-        #    context.post_handshake_auth = True
-        # context.check_hostname=False;
+        DOMAIN_API=matches[0];
 
-        with socket.create_connection(('api-plhq.tvhaystream.xyz', 443)) as sock:
-            with context.wrap_socket(sock,
-                                     server_hostname='api-plhq.tvhaystream.xyz') as sslsock:
-                dercert = sslsock.getpeercert(True)
-        cert = ssl.DER_cert_to_PEM_cert(dercert)
+        matches = re.findall(r'((?P<SCHEME>(?:https?))\:\/\/(?P<HOSTNAME>(?:www.|[a-zA-Z0-9.]+)[a-zA-Z0-9\-\.]+\.([a-zA-Z0-9]*))(?P<PORT>\:[0-9][0-9]{0,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?P<PATH>[a-zA-Z0-9\-\.\/]+)?(?P<QUERY>(?:\?$|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?(?P<FRAGMENT>#[a-zA-Z0-9\-\.]+)?)'
+        ,DOMAIN_API);
+        hostname=matches[0][2];
 
-        # prepare configuration
-        configuration = QuicConfiguration(
-            is_client=True, alpn_protocols=H3_ALPN
-        )
-        configuration.load_verify_locations(cadata=bytes(cert, 'utf-8'))
-        connection = QuicConnection(configuration=configuration);
+
+
+        apisingok=self._download_webpage('https://api-sing-02.tvhaystream.xyz/apiv2/views/%s'%video_id,show_path+'bis');
         # connect
         addr = '%s%s/%s' % (DOMAIN_API,idUser,video_id)#"https://api-plhq.tvhaystream.xyz/apiv4/5ee31dd5665f2d19d5af4a99/6121dce66cfe6c0a52ca86de";
-        stream_handler = None
-        local_host = "::"
-        local_port = 31280
-        loop = asyncio.get_event_loop()
+        #addr = "https://api-plhq.tvhaystream.xyz/apiv4/5ee31dd5665f2d19d5af4a99/6121dce66cfe6c0a52ca86de";
+        #addr = '%s%s/%s' % ("https://api-plhq.tvhaystream.xyz/apiv4/", idUser,    video_id)  # "https://api-plhq.tvhaystream.xyz/apiv4/5ee31dd5665f2d19d5af4a99/6121dce66cfe6c0a52ca86de";
 
-        future = loop.run_until_complete(
-            # asyncio.run(
-            runhttp3request(
-                #      addr,
-                # 443,
-                #     configuration=configuration,
-                #     create_protocol=QuicConnectionProtocol,
-                #     session_ticket_handler=None,
-                #     local_port=local_port,
-                #     wait_connected=True
-                configuration,
-                [addr],
-                # 'referrer=http://%3A%2F%2Ftvhai.org&typeend=html',
-                'referrer=http://tvhai.org&typeend=html',
-                False,
-                local_port,
-                True,
-            ));
+        requestresult='';
 
-        # print("func_normal()={future}".format(**vars()))
+        if False: # http3 path not here anymore
+
+            print('Contact Http3 %s'%addr)
+            # print( '%s%s/%s' % (DOMAIN_API,idUser,video_id));
+            context = ssl._create_default_https_context()
+            # enable PHA for TLS 1.3 connections if available
+            # if context.post_handshake_auth is not None:
+            #    context.post_handshake_auth = True
+            # context.check_hostname=False;
+
+            with socket.create_connection((hostname, 443)) as sock:
+                with context.wrap_socket(sock,
+                                         server_hostname=hostname) as sslsock:
+                    dercert = sslsock.getpeercert(True)
+            cert = ssl.DER_cert_to_PEM_cert(dercert)
+
+            # prepare configuration
+            configuration = QuicConfiguration(
+                is_client=True, alpn_protocols=H3_ALPN
+            )
+            configuration.load_verify_locations(cadata=bytes(cert, 'utf-8'))
+            connection = QuicConnection(configuration=configuration);
+            stream_handler = None
+            local_host = "::"
+            local_port = 31280
+            loop = asyncio.get_event_loop()
+
+            future = loop.run_until_complete(
+                # asyncio.run(
+                runhttp3request(
+                    #      addr,
+                    # 443,
+                    #     configuration=configuration,
+                    #     create_protocol=QuicConnectionProtocol,
+                    #     session_ticket_handler=None,
+                    #     local_port=local_port,
+                    #     wait_connected=True
+                    configuration,
+                    [addr],
+                    #'referrer=http://%3A%2F%2Ftvhai.org&typeend=html',
+                    'referrer=http://tvhai.org&typeend=html',
+                    False,
+                    local_port,
+                    True,
+                ));
+            requestresult=future[0]
+            loop.close()
+
+            VALID_M3U = 'https?://(?:www\.)?(([\w^\/-]*)\/)*(?P<id>[^?#&]+)\.m3u8'
+            matches = re.search(VALID_M3U,requestresult )
+            m3u8url = matches[0];
+            formats = self._extract_m3u8_formats(
+                m3u8url,
+                video_id, 'mp4')
+             #  https://m3u8-plhq.tvhaystream.xyz/m3u8/v3/5/6121dce66cfe6c0a52ca86de/1630510274/b7bfab92ee090d3ddce2989f62e2b1fd.m3u8
+        else:
+            headers = {
+                # 'Host': 'api-if.tvhaystream.xyz',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+                'Accept': '*/*',
+                'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
+                'Accept-Encoding': 'gzip, deflate',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                # 'Content-Length': '44',
+                'Connection': 'keep-alive',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'cross-site',
+                'Pragma': 'no-cache',
+                'Cache-Control': 'no-cache',
+                # 'Authorization': '%s %s' % (token_type, access_token),
+                # 'Referer': '%s' % ('https://play.tvhaystream.xyz'),
+                'Origin': '%s' % ('https://play.tvhaystream.xyz'),
+            }
+            videojsonhandle = self._download_json_handle(
+                addr ,
+                show_path, 'Downloading video JSON',
+                data=bytes('referrer=http://tvhai.org&typeend=html', 'utf-8'), headers=   headers , fatal=False)
+
+
+            requestresult= videojsonhandle[0];
+            print(requestresult);
+
+
+            m3u8_doc='#EXTM3U\n';
+            m3u8_doc+='#EXT-X-VERSION:3\n';
+            m3u8_doc+='#EXT-X-TARGETDURATION:10\n';
+            m3u8_doc+='#EXT-X-PLAYLIST-TYPE:VOD\n';
+            # EXTM3U
+            # EXT-X-VERSION:3
+            # EXT-X-TARGETDURATION:10
+            # EXT-X-PLAYLIST-TYPE:VOD
+            # EXTINF:10,
+            headers = {'Accept': '*/*',
+                       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0',
+                       "Origin": "https://play.tvhaystream.xyz",  "referer": "https://apird-tvhai.rdgogo.xyz",
+                       "Sec-Fetch-Site": "cross-site", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Dest": "empty",
+                       "Accept": "*/*", "Accept-Encoding": "gzip, deflate", "TE": "trailers",
+                       "Accept-Language": "fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3", "Pragma": "no-cache", "DNT": "1",
+                       "Sec-GPC": "1", "Connection": "keep-alive"}
+            headreqs=[];
+
+            idx=0
+            while idx <len (requestresult['data'][1]):
+                id2=0
+                headreqs2=[]
+                while (id2+idx<len (requestresult['data'][1]) ) and (id2<50) :
+                    v=requestresult['data'][1][idx+id2]
+                    thr=HeadRequest('https://apird-tvhai.rdgogo.xyz/rdv5/%s/%s/%s.rd' % (requestresult['quaity'], idUser, v),v,headers,self);
+                    thr.set_downloader(self._downloader)
+                    headreqs2.append(thr);
+                    id2=id2+1;
+                for r in headreqs2:
+                    r.start();
+                for r in headreqs2:
+                    r.join();
+                headreqs.extend(headreqs2);
+                idx=idx+id2;
+
+                # for v in requestresult['data'][1]:
+                #     thr=HeadRequest('https://apird-tvhai.rdgogo.xyz/rdv5/%s/%s/%s.rd' % (requestresult['quaity'], idUser, v),v,headers,self);
+                #     thr.set_downloader(self._downloader)
+                #     headreqs.append(thr);
+
+
+            idx=0
+            for v in requestresult['data'][1]:
+                v1= re.findall('\w+$',v);
+                m3Uchunk='https://apird-tvhai.rdgogo.xyz/rdv5/%s/%s/%s.rd' %(requestresult['quaity'],idUser,v)
+
+                # metachunk = self._request_webpage(m3Uchunk, show_path, #'note', 'errnote', fatal, data=data,
+                #                                    headers=headers,
+                #                           #   query=query, expected_status=expected_status
+                #                                   )
+                #
+                # print(metachunk.headers['content-length'])
+                chunksize=int(headreqs[idx].responseheader.headers['content-length']);
+
+                #print(m3Uchunk);
+                m3u8_doc +='#EXTINF:%s,\n' % requestresult['data'][0][idx];
+                m3u8_doc +='#EXT-X-BYTERANGE:%d@8\n'%chunksize;
+                m3u8_doc += m3Uchunk+'\n';
+                idx=idx+1
+            m3u8_doc +='#EXT-X-ENDLIST\n';
+            print(m3u8_doc)
+            d=base64.b64encode(bytes(m3u8_doc, 'UTF-8'))
+            dataurl='data:application/x-mpegURL;base64,' +d.decode(encoding="utf-8");
+            formats = self._extract_m3u8_formats(
+            dataurl,
+               video_id, 'mp4',entry_protocol='m3u8_native')
+
+            # formats = self._parse_m3u8_formats(
+            #     'data://application/x-mpegURL,'+m3u8_doc, video_id,
+            #     # ext=mp4, entry_protocol=entry_protocol,
+            #     # preference=preference, m3u8_id=m3u8_id, live=live
+            # )
+            #requestresult=videojsonhandle[0];
+
+            # print("func_normal()={future}".format(**vars()))
         #print(future[0])
         # r'https?://(?:www\.)?tvhai\.org(([\w^\/-]*)\/)*(?P<id>[^\/?#&]+)'
 
-        VALID_M3U = 'https?://(?:www\.)?(([\w^\/-]*)\/)*(?P<id>[^?#&]+)\.m3u8'
-        matches = re.search(VALID_M3U, future[0])
-        m3u8url = matches[0];
-        loop.close()
-
-        formats = self._extract_m3u8_formats(
-            m3u8url,
-            video_id, 'mp4')
+        # VALID_M3U = 'https?://(?:www\.)?(([\w^\/-]*)\/)*(?P<id>[^?#&]+)\.m3u8'
+        # matches = re.search(VALID_M3U,requestresult )
+        # m3u8url = matches[0];
+        # formats = self._extract_m3u8_formats(
+        #     m3u8url,
+        #     video_id, 'mp4')
         #    https://m3u8-plhq.tvhaystream.xyz/m3u8/v3/5/6121dce66cfe6c0a52ca86de/1630510274/b7bfab92ee090d3ddce2989f62e2b1fd.m3u8
+
+
         self._sort_formats(formats)
         return {
             'id': video_id,
